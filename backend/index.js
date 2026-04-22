0db4c221bed48ef6551e2c99c6cb9046091d8fc9
Harkiran Kaur <harkirankaur.0606@gmail.com>
2026-04-22 13:28:19 +0530

    changed port for deployment

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,8 @@ app.use("/api/security", securityRoutes);
 
 require("./jobs/transaction.worker");
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
